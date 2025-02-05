9356397755b2110f427dc357d7a27c4aceb69120
JatinG <gurwanianj975@gmail.com>
2025-02-05 23:47:21 +0530

    Created Web Interface

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask,render_template,request,jsonify
+from flask import Flask,render_template,request,jsonify,session,redirect,url_for
+from flask_bootstrap import Bootstrap5
 from flask_smorest import Api
+from flask_wtf import CSRFProtect
 from models import db, usermodel
-from resources import blp_api_user,blp_api_notes,blp_api_access
+from resources import blp_api_user,blp_api_notes,blp_api_access,blp_app_user,blp_app_notes
 from sqlalchemy.exc import SQLAlchemyError,IntegrityError
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
+import urllib.parse
 
 app = Flask(__name__)
 app_url_prefix = '/app'
@@ -25,7 +28,8 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db.init_app(app)
 migrate=Migrate(app,db)
 
-
+bootstrap = Bootstrap5(app)
+app.config["SECRET_KEY"] = "Notes Flask App"
 app.config["JWT_SECRET_KEY"] = "Notes Flask App"
 jwt = JWTManager(app)
 
@@ -63,9 +67,32 @@ API = Api(app)
 API.register_blueprint(blp_api_user)
 API.register_blueprint(blp_api_notes)
 API.register_blueprint(blp_api_access)
+API.register_blueprint(blp_app_user,url_prefix = '/')
+API.register_blueprint(blp_app_notes,url_prefix = '/app')
 
+@app.errorhandler(404)
+def page_not_found(e = None):
+    if request.path.startswith('/api/'):
+         return jsonify({"status": "Not Found","code": 404}),404
+    else:
+        return render_template('404.html'),404
 
-@app.route('/')
-def introduction():
-    return render_template('Home.html')
+@app.errorhandler(405)
+def method_not_found(e = None):
+    if request.path.startswith('/api/'):
+         return jsonify({"status": "Method Not Allowed","code": 405}),405
+    else:
+        return render_template('404.html'),405
 
+@app.errorhandler(500)
+def Internal_server_error(e = None,message = "Internal server error"):
+    if request.path.startswith('/api/'):
+         return jsonify({"status": "internal server error","code": 500 , "message":message}),500
+    else:
+        if e is not None:
+            print("Error 500 : ",e)
+        return render_template('500.html'),500
+    
+@app.template_filter('quote')
+def quote_filter(s):
+     return urllib.parse.quote(s)
---
+++ resources/app_user.py
@@ -0,0 +1,93 @@
+from flask import Flask,render_template,redirect,url_for,session,request,flash
+from flask.views import MethodView
+from models import db,usermodel
+from flask_smorest import Blueprint
+from passlib.hash import pbkdf2_sha256
+from sqlalchemy.exc import SQLAlchemyError,IntegrityError
+from resources.forms import RegisterForm,LoginForm
+
+
+blp = Blueprint('APP_USER',__name__,template_folder='templates' ,description='User Management Operations for Web Interface')
+
+@blp.route('/')
+class app_home(MethodView):
+    def get(self):
+        try:
+            if session['user_logged_in']:
+                flash('Welcome Back ', 'info')
+                return redirect(url_for('APP_NOTES.all_notes'))
+            return render_template('home.html')
+        except Exception as e:
+            return render_template('home.html')
+    
+@blp.route('/register')
+class app_register(MethodView):
+
+    
+    def get(self):
+        form = RegisterForm()
+        return render_template('register.html',form= form)
+    
+    def post(self):
+        form = RegisterForm()
+        if form.validate_on_submit():
+            username = request.form['username']
+            password = request.form['password']
+            user = usermodel.query.filter(usermodel.username == username).first()
+            if user:
+                return render_template('register.html',error="User Already Exist")
+            else:
+                try:
+                    user_password_encrypted = pbkdf2_sha256.hash(password)
+                    new_user = usermodel(username = username,password = user_password_encrypted)
+                    db.session.add(new_user)
+                    db.session.commit()
+                    flash('User Registered Successfully', 'info')
+                    return redirect(url_for('APP_USER.app_login'))
+                except IntegrityError as e:
+                    flash('User already exist', 'error')
+                    return render_template('register.html',error="User Already Exist",form= form)
+                except SQLAlchemyError as e:
+                    flash('Your request has been failed , please try again later', 'error')
+                    return render_template('register.html',error="unable to add user details into backend",form= form)
+                except Exception as e:
+                    return render_template('register.html',error="unable to add user details into backend",form= form)
+        else:
+            return render_template('register.html',form= form)
+    
+@blp.route('/login')
+class app_login(MethodView):
+    def get(self):
+        form = LoginForm()
+        return render_template('login.html',form= form)
+    
+    def post(self):
+        form = LoginForm()
+        if form.validate_on_submit():
+            username = request.form['username']
+            password = request.form['password']
+            user = usermodel.query.filter(usermodel.username == username).first()
+            if user and pbkdf2_sha256.verify(password, user.password):
+                session['user_logged_in']=True
+                session['user_name']=username
+                session['user_id']=user.id
+                flash('you are logged in  successfully', 'info')
+                return redirect(url_for('APP_NOTES.all_notes'))
+            else:
+                flash('Invalid username or password', 'error')
+                return render_template('login.html',form= form)
+        else:
+            flash('Your request has been failed , please try again later', 'error')
+            return render_template('login.html',form= form)
+        
+@blp.route('/logout')
+class app_logout(MethodView):
+    def get(self):
+        session.clear()
+        flash('You have been Logged out Successfully', 'info')
+        return redirect(url_for('APP_USER.app_home'))
+    
+    
+        
+
+    
